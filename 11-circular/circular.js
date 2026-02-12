f816a3fb627fdc4a498fd261a9828a94f7166b66
StephanosNt <s.ntentopoulos@gmail.com>
2026-02-12 20:56:33 +0200

    JS Piscine Circular

---
+++ 11-circular/circular.js
@@ -0,0 +1,6 @@
+//Create an empty object and assign it to the variable "circular"
+const circular = {};
+
+//Add a property named circular to the object that points back to itself
+circular.circular = circular;
+

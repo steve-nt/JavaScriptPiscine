383c1dd7966b3d144cb21f95b644d1d0e99f635e
StephanosNt <s.ntentopoulos@gmail.com>
2026-02-12 21:25:14 +0200

    JS Piscine Biggie-Smalls

---
+++ 12-biggie-smalls/biggie-smalls.js
@@ -0,0 +1,8 @@
+// 'smalls' holds the smallest possible number JavaScript can represent -Infinity represents the mathematical concept of negative infinity It's the lower boundary of all numbers [mallest possible number value (considering negatives)]
+const smalls = -Infinity;
+
+//Alternative the smallest finite number: -Number.MAX_VALUE
+
+// 'biggie' holds the largest possible number JavaScript can represent Number.MAX_VALUE is the largest finite number in JavaScript (Infinity exists, but MAX_VALUE is the largest "regular" number)
+const biggie = Number.MAX_VALUE;
+
